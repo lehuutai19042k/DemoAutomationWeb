cdd11674ec0260586a67cba8ae5622ec371388b1
lehuutai19042k <lehuutai19042k@gmail.com>
2024-08-16 14:11:26 +0700

    PageObjectModel: comment row 53

--- src/TestCase/TestDemo.java
+++ src/TestCase/TestDemo.java
@@ -50,7 +50,7 @@ public class TestDemo extends BaseSetup {
 
         dasboardPage.selectAndCurrency();
         //dasboardPage.verifySearchCurrency();
-//        dasboardPage.selectAndDefaultLanguage();
+        dasboardPage.selectAndDefaultLanguage();
 
         dasboardPage.selectFillCountry();
 
